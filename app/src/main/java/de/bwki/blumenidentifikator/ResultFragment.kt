ba2818c4fdd172889b6493e354be2cc2a5daf4fc
Madokami <fabianweller7@gmail.com>
2019-06-06 19:06:08 +0200

    Model eingebaut!

--- app/src/main/java/de/bwki/blumenidentifikator/ResultFragment.kt
+++ app/src/main/java/de/bwki/blumenidentifikator/ResultFragment.kt
@@ -3,11 +3,13 @@ package de.bwki.blumenidentifikator
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -19,17 +21,22 @@ import de.bwki.blumenidentifikator.databinding.FragmentResultBinding
  *
  */
 
-class ResultFragment : Fragment(), MainActivity.GlobalMethods {
+class ResultFragment : Fragment(), MainActivity.GlobalMethods{
 
     lateinit var viewModel: ResultScreenModel
+    private lateinit var classifier: ImageClassifier
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentResultBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
-        val resultScreenModel = ViewModelProviders.of(this).get(ResultScreenModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(ResultScreenModel::class.java)
+        viewModel.loadModule()
+        viewModel.loadModule2()
+
 
         Log.d("ResultFragment", "OnCreateView called")
         setHasOptionsMenu(true)
@@ -37,14 +44,39 @@ class ResultFragment : Fragment(), MainActivity.GlobalMethods {
         // Hole Bilddatei und konvertiere es in Bitmap, damit es angezeigt werden kann.
         // HINWEIS: ImageView ist um 90 Grad gedreht, um Bild gerade anzuzeigen.
         // HINWEIS: Bilder werden im Format YUV_422_888 gespeichert
-        val filePath = arguments!!.getString("file")
-        val bitmap: Bitmap = BitmapFactory.decodeFile(filePath)
-        binding.imageView.setImageBitmap(bitmap)
+        //val bitmap:Bitmap? = arguments!!.getParcelable("image")
+        //binding.imageView.setImageBitmap(bitmap)
 
-        binding.resultScreenModel = resultScreenModel
+        //viewModel.classify(bitmap!!)
+        val imageFile = arguments!!.getString("file")
+        val bitmapResized = Bitmap.createScaledBitmap(
+            BitmapFactory.decodeFile(imageFile),
+            DEFAULT_INPUT_SIZE,
+            DEFAULT_INPUT_SIZE,
+            false
+        )
+        Log.e(
+            "Image",
+            "Height ${bitmapResized!!.height} Width ${bitmapResized.width} Color ${bitmapResized.colorSpace}"
+        )
+
+        binding.imageView.setImageBitmap(bitmapResized)
+         viewModel.classify(bitmapResized)
+
+        binding.resultScreenModel = viewModel
         binding.lifecycleOwner = this
+
+/*        classifier = ImageClassifier(getAsset())
+        classifier.recognizeImage(bitmapResized).subscribeBy(
+            onSuccess = {
+                binding.resultObject.text = it.toString()
+            }
+        )*/
         return binding.root
     }
+
+
+
 
     //TODO? Die Ergebnisse mithilfe eines RecyclerViews und Adapter anzeigen lassen?
     //TODO Falls Internetverbindung weitere Informationen über Blume aus dem Internet holen
